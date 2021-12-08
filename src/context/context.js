1bf8825e6ced4a01ff84025825a9d8297d9fc182
Vjola <vjolashtishi@gmail.com>
2021-12-08 13:11:39 +0100

    Fetching user questions/answers dynamically

--- src/context/context.js
+++ src/context/context.js
@@ -5,8 +5,7 @@ import mockAnswers from './mockData/mockAnswers'
 
 import axios from 'axios'
 
-const rootUrl =
-  'https://api.stackexchange.com/2.3/users?order=desc&sort=reputation&site=stackoverflow'
+const rootUrl = 'https://api.stackexchange.com/2.3/users'
 
 const StackExchangeContext = React.createContext()
 
@@ -23,19 +22,27 @@ const StackExchangeProvider = ({ children }) => {
   }
 
   const searchStackExchangeUser = async (user) => {
-    toggleError();
-    setIsLoading(true);
+    toggleError()
+    setIsLoading(true)
     if (user) {
-      const response = await axios(`${rootUrl}&inname=${user}`).catch((err) =>
-        console.log(err)
-      )
+      const response = await axios(
+        `${rootUrl}?order=desc&sort=reputation&site=stackoverflow&inname=${user}`
+      ).catch((err) => console.log(err))
       if (response.data.items.length > 0) {
-        console.log(response.data.items[0])
         setStackExchangeUser(response.data.items[0])
+        const { user_id } = response.data.items[0]
+        const questionsResponse = await axios(
+          `${rootUrl}/${user_id}/questions?order=desc&sort=activity&site=stackoverflow`
+        ).catch((err) => console.log(err))
+        setQuestions(questionsResponse.data)
+        const answersResponse = await axios(
+          `${rootUrl}/${user_id}/answers?order=desc&sort=activity&site=stackoverflow`
+        )
+        setAnswers(answersResponse.data)
       } else {
         toggleError(true, 'No user found')
       }
-      setIsLoading(false);
+      setIsLoading(false)
     }
   }
 
@@ -50,7 +57,7 @@ const StackExchangeProvider = ({ children }) => {
         answers,
         error,
         searchStackExchangeUser,
-        isLoading
+        isLoading,
       }}
     >
       {children}
