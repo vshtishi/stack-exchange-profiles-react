413e4e552c34a32379ded0c96149c2ae2a21ef2f
Vjola <vjolashtishi@gmail.com>
2021-12-09 06:44:39 +0100

    wip: adding authentication

--- src/index.js
+++ src/index.js
@@ -1,21 +1,26 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
-import { StackExchangeProvider } from './context/context';
-import { Auth0Provider } from '@auth0/auth0-react';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
+import App from './App'
+import * as serviceWorker from './serviceWorker'
+import { StackExchangeProvider } from './context/context'
+import { Auth0Provider } from '@auth0/auth0-react'
 
 ReactDOM.render(
   <React.StrictMode>
-    <StackExchangeProvider>
-      <App />
-    </StackExchangeProvider>
+    <Auth0Provider
+      domain='dev-1qmjaj6c.us.auth0.com'
+      clientId='D5PWSjJDxC0zPlO2br7dQbNjcntWwi3Y'
+    >
+      <StackExchangeProvider>
+        <App />
+      </StackExchangeProvider>
+    </Auth0Provider>
   </React.StrictMode>,
   document.getElementById('root')
-);
+)
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+serviceWorker.unregister()
